8c7e2aa01bda709fcaa7937837ad071bf51d5ed2
rubens-lavor <haryson07@gmail.com>
2020-08-13 10:54:17 -0300

    model de produto

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 //iniciando o App
 const app = express();
@@ -7,9 +8,24 @@ const app = express();
 //iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true , useUnifiedTopology: true});
 
+//model registrado na aplicação
+// require('./src/models/Product');
+
+//com o requireDir
+requireDir("./src/models");
+
+//verificando se consegue salvar algo no bando de dados
+const Product = mongoose.model("Product");
+
 //Primeira rota
 app.get('/',(req, res) => {
-    res.send("hello word!!!")
+
+    Product.create({
+        title: "React Native",
+        description: "Build native apps with React",
+        url: "https://github.com/facebook/react-native"
+    })
+    return res.send("hello word!!!")
 });
 
 app.listen(3001);
