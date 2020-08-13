c40bd9518f1ab5917722dc2b8fd92842e8cfd2d3
rubens-lavor <haryson07@gmail.com>
2020-08-13 10:36:09 -0300

    databaase conectado

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//iniciando o App
 const app = express();
 
+//iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true , useUnifiedTopology: true});
+
+//Primeira rota
 app.get('/',(req, res) => {
     res.send("hello word!!!")
 });
