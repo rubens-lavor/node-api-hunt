877a5a03ddf5c9883a534023e36bf6f52466bae8
rubens-lavor <haryson07@gmail.com>
2020-08-13 12:06:48 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,16 +3,45 @@ const mongoose =  require('mongoose');
 const Product = mongoose.model("Product");
 
 module.exports = {
+
+    //rota de listagem
     async index(req, res) {
         const products = await Product.find();
 
         return res.json(products);
     },
 
+    //rota de detalhes
+    async show(req, res) {
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
+    //rota de criação
     async store(req, res) {
-        //criação
         const product = await Product.create(req.body);
 
         return res.json(product);
+    },
+
+    //rota de update
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true});
+
+        return res.json(product);
+    },
+
+    //rota de remover
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+
     }
+
 };
+
+/**
+ * CRUD (acrónimo do inglês Create, Read, Update and *Delete) são as quatro operações básicas (criação, *consulta, atualização e destruição de dados) utilizadas em *bases de dados relacionais
+ */
--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,18 @@ const ProductController = require("./controllers/ProductController");
 
 
 routes.get("/products", ProductController.index);
+
+routes.get("/products/:id", ProductController.show);
+
 //.post sempre que for criar algo
 routes.post("/products", ProductController.store);
+
+//para criar a rota de atualização usa-se o método put
+//tbm precisa do id, aí é só chamar o método update
+routes.put("/products/:id", ProductController.update);
+
+
+routes.delete("/products/:id", ProductController.destroy);
 
 
 module.exports = routes;
