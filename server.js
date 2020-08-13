390b026ab6d0b224460e5940f258c01068ec49fe
rubens-lavor <haryson07@gmail.com>
2020-08-13 11:17:27 -0300

    retorno do registro bd, reestruturação de arquivos

--- server.js
+++ server.js
@@ -14,18 +14,7 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true , u
 //com o requireDir
 requireDir("./src/models");
 
-//verificando se consegue salvar algo no bando de dados
-const Product = mongoose.model("Product");
-
-//Primeira rota
-app.get('/',(req, res) => {
-
-    Product.create({
-        title: "React Native",
-        description: "Build native apps with React",
-        url: "https://github.com/facebook/react-native"
-    })
-    return res.send("hello word!!!")
-});
+//rotas
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
